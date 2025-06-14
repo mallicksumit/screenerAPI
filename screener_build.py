25174c5fe1123a108b0bb497884a733f4c422029
Sumit Mallick <mallick.23.sumit@gmail.com>
2025-06-14 18:06:25 +0530

    Update screener_build.py

--- screener_build.py
+++ screener_build.py
@@ -36,12 +36,18 @@ import os
 import time
 import urllib.parse
 import pandas as pd
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 
 app = Flask(__name__)
 
