6e2979e7b9d9b57d122f99b35757fa0bf7d32851
Sumit Mallick <mallick.23.sumit@gmail.com>
2025-06-14 17:01:20 +0530

    Update screener_build.py

--- screener_build.py
+++ screener_build.py
@@ -30,11 +30,6 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--disable-gpu')
 
-driver = webdriver.Chrome(options=chrome_options)
-driver.get('https://www.google.com')
-print(driver.title)
-
-driver.quit()
 
 from flask import Flask, request, jsonify
 import os
